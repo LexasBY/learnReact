101fc986221284af05647fee0dee053b81f16788
LEXASBY <lexasby@MacBook-Pro-LEXASBY.local>
2022-05-04 11:28:18 +0300

    add some components

--- todo/src/index.js
+++ todo/src/index.js
@@ -1,1 +1,31 @@
-console.log('go fuck yourself')
+import React from 'react'
+import ReactDOM from 'react-dom'
+
+const TodoList = () => {
+  return (
+    <ul>
+    <li>Learn React</li>
+    <li>Build Awesome App</li>
+  </ul>
+  )
+}
+const AppHeader = () => {
+  return <h1>My ToDo List</h1>
+}
+const SearchPanel = () => {
+  return <input placeholder='search'/>
+}
+const App = () => {
+  return (
+  <div>
+    <AppHeader/>
+    <SearchPanel/>
+    <TodoList/>
+  </div>
+  )
+}
+const el = <App/>
+
+ReactDOM.render(el, document.querySelector('#root'))
+
+
