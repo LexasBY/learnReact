446571a98fb820a9140916e2ebf1a78d803428bb
LEXASBY <lexasby@MacBook-Pro-LEXASBY.local>
2022-05-05 17:26:31 +0300

    resolve key issue

--- todo/src/index.js
+++ todo/src/index.js
@@ -1,14 +1,17 @@
 import React from 'react'
-import ReactDOM from 'react-dom'
+import {createRoot} from 'react-dom/client';
 import AppHeader from './components/app-header'
 import TodoList from './components/todo-list'
 import SearchPanel from './components/search-panel'
 
+const rootElement = document.getElementById('root');
+const root = createRoot(rootElement);
+
 const App = () => {
   const todoData = [
-    { label: 'Drink Coffee', important: false},
-    { label: 'Make Awesomme App', important: true},
-    { label: 'Have a lunch', important: false},
+    { label: 'Drink Coffee', important: false, id: 1},
+    { label: 'Make Awesomme App', important: true, id: 2},
+    { label: 'Have a lunch', important: false, id: 3},
   ]
   return (
   <div>
@@ -19,7 +22,6 @@ const App = () => {
   )
 }
 const el = <App/>
-
-ReactDOM.render(el, document.querySelector('#root'))
+root.render(el)
 
 
